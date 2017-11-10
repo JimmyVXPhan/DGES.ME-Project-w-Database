d9b01416217468a5062d582785b33c29a9412d4d
ragan2 <ragan1@student.gsu.edu>
2017-11-09 22:53:41 -0500

    Update login.inc.php

--- _includes/login.inc.php
+++ _includes/login.inc.php
@@ -6,36 +6,34 @@ if (isset($_POST['submit'])) {
 
 	include 'dbh.inc.php';
 
-	$uid = mysqli_real_escape_string($conn, $_POST['uid']);
-	$pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+	$username = mysqli_real_escape_string($conn, $_POST['username']);
+	$password= mysqli_real_escape_string($conn, $_POST['password']);
 
 	//Error handlers
 	//Check if inputs are empty
-	if (empty($uid) || empty($pwd)) {
-		header("Location: ../mainPage.php?login=empty");
+	if (empty($username) || empty($password)) {
+		header("Location: ../index.php?login=empty");
 		exit();
 	} else {
-		$sql = "SELECT * FROM users WHERE user_uid='$uid' OR user_email='$uid'";
+		$sql = "SELECT * FROM users WHERE user_username='$username' OR user_email='$username'";
 		$result = mysqli_query($conn, $sql);
 		$resultCheck = mysqli_num_rows($result);
 		if ($resultCheck < 1) {
-			header("Location: ../mainPage.php?login=error");
+			header("Location: ../index.php?login=error");
 			exit();
 		} else {
 			if ($row = mysqli_fetch_assoc($result)) {
 				//De-hashing the password
-				$hashedPwdCheck = password_verify($pwd, $row['user_pwd']);
-				if ($hashedPwdCheck == false) {
-					header("Location: ../mainPage.php?login=error");
+				$hashedpasswordCheck = password_verify($password, $row['user_password']);
+				if ($hashedpasswordCheck == false) {
+					header("Location: ../index.php?login=error");
 					exit();
-				} elseif ($hashedPwdCheck == true) {
+				} elseif ($hashedpasswordCheck == true) {
 					//Log in the user here
 					$_SESSION['u_id'] = $row['user_id'];
 					$_SESSION['u_first'] = $row['user_first'];
-					$_SESSION['u_last'] = $row['user_last'];
-					$_SESSION['u_email'] = $row['user_email'];
-					$_SESSION['u_uid'] = $row['user_uid'];
-					header("Location: ../mainPage.php?login=success");
+					$_SESSION['u_last'] = $row['user_last'];		
+					header("Location: ../index.php?login=success");
 					phpAlert("Success");
 					exit();
 				}
@@ -44,7 +42,7 @@ if (isset($_POST['submit'])) {
 	}
 
 } else {
-	header("Location: ../mainPage.php?login=error");
+	header("Location: ../index.php?login=error");
 	exit();
 }
 
