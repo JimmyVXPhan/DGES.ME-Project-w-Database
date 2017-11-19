7d3d6a14262baa491b784132c07ef14acf39abfa
ragan2 <ragan1@student.gsu.edu>
2017-11-18 23:31:04 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,8 +3,31 @@
     include '_includes/header.php';
 ?>
 
-<title>Contact Us</title>
+<?php
+if(isset($_POST['submit'])){
+    $to = "support@dges.me"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
+
+<!DOCTYPE HTML>
+<head>
+<title>Contact Us</title>
+</head>
   <body>
     <style>
       .form-area
@@ -19,27 +42,7 @@
         margin: auto;
       }
     </style>
-    <script>
-        $('ul li a').click(function(){ $('li a').removeClass("active"); $(this).addClass("active"); });
-        $(document).ready(function(){
-        $('#characterLeft').text('140 characters left');
-        $('#message').keydown(function () {
-            var max = 140;
-            var len = $(this).val().length;
-            if (len >= max) {
-                $('#characterLeft').text('You have reached the limit');
-                $('#characterLeft').addClass('red');
-                $('#btnSubmit').addClass('disabled');
-            }
-            else {
-                var ch = max - len;
-                $('#characterLeft').text(ch + ' characters left');
-                $('#btnSubmit').removeClass('disabled');
-                $('#characterLeft').removeClass('red'); 
-            }
-        });
-    });
-    </script>
+  
 
   <div class="container">
     <br>
@@ -48,27 +51,25 @@
     <div class="container">
       <div class="col-md-5 center">
           <div class="form-area" >
-              <form role="form">
+              <form role="form" action="" method="post">
               <br style="clear:both">
-                          <h3 style="margin-bottom: 25px; text-align: center;">Contact Form</h3>
+                          <h4 style="margin-bottom: 25px; text-align: center;">Contact Form</h4>
           				<div class="form-group">
-      						<input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
+      						<input type="text" class="form-control"  name="first_name" placeholder="Name" required>
+      					</div>
+					<div class="form-group">
+      						<input type="text" class="form-control"  name="last_name" placeholder="Last Name" required>
       					</div>
       					<div class="form-group">
-      						<input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
+      						<input type="text" class="form-control" name="email" placeholder="Email" required>
       					</div>
-      					<div class="form-group">
-      						<input type="text" class="form-control" id="mobile" name="mobile" placeholder="Mobile Number" required>
-      					</div>
-      					<div class="form-group">
-      						<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required>
-      					</div>
+      				  
                           <div class="form-group">
-                          <textarea class="form-control" type="textarea" id="message" placeholder="Message" maxlength="140" rows="7"></textarea>
-                              <span class="help-block"><p id="characterLeft" class="help-block ">You have reached the limit</p></span>
+                          <textarea class="form-control" type="textarea"  name="message" placeholder="Message" maxlength="140" rows="7"></textarea>
+                            
                           </div>
 
-              <button type="button" id="submit" name="submit" class="btn btn-primary pull-right">Submit Form</button>
+              <input type="submit" name="submit" value="Submit">
               </form>
           </div>
         </div>
