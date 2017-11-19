0cac231a7fc1a4e5c28d0f1f87e6e689f32cfccc
ragan2 <ragan1@student.gsu.edu>
2017-11-18 23:39:05 -0500

    Update resetPassword.php

--- resetPassword.php
+++ resetPassword.php
@@ -1,43 +1,57 @@
 <?php
-    $active = "Contact";
+    
     include '_includes/header.php';
 ?>
 
-<title>Forgot Password</title>
 
-<body>
+<!DOCTYPE HTML>
+<head>
+<title>Reset Password</title>
+</head>
+  <body>
+    <style>
+      .form-area
+      {
+      	padding: auto 40px 60px;
+      	margin: 0px 75px 60px;
+      	}
+        .center {
+        position: absolute;
+        left: 0;
+        right: 0;
+        margin: auto;
+      }
+    </style>
+  
 
-  <br>
-  <br>
   <div class="container">
-    <div class="col-md-5">
-    <form>
-      <div class="form-group row">
-        <label for="inputEmail3" class="col-sm-2 col-form-label">New Password</label>
-        <div class="col-sm-10">
-          <input type="email" class="form-control" id="inputEmail3" placeholder="Password">
+    <br>
+    <br>
+    <h2 style="text-align: center">Reset your Password</h2>
+    <div class="container">
+      <div class="col-md-5 center">
+          <div class="form-area" >
+              <form role="form" action="_includes/reset.inc.php" method="post">
+              <br style="clear:both">
+                          <h4 style="margin-bottom: 25px; text-align: center;">Enter the email address associated with your DGES.me account</h4>
+          				<div class="form-group">
+      						<input type="text" class="form-control" name="email" placeholder="Email" required>
+      					</div>
+      				   					
+      					<div class="form-group">
+      						<input type="text" class="form-control"  name="newpassword" placeholder="New Password" required>
+      					</div>
+					<div class="form-group">
+      						<input type="text" class="form-control"  name="confirmpassword" placeholder="Confirm Password" required>
+      					</div>
+      					
+      					
+      				  
+                       <input type="submit" name="submit" value="Submit">
+              </form>
+          </div>
         </div>
       </div>
-      <div class="form-group row">
-        <label for="inputPassword3" class="col-sm-2 col-form-label"></label>
-        <div class="col-sm-10">
-          <input type="password" class="form-control" id="inputPassword3" placeholder="Confirm Password">
-        </div>
-      </div>
-      <div class="form-group row">
-        <div class="offset-sm-2 col-sm-10">
-          <button type="submit" class="btn btn-primary">Reset</button>
-        </div>
-      </div>
-    </form>
-  </div>
-
-  </div>
-
-
-</body>
-
-
-
-
+    </div>
+  </body>
 </html>
