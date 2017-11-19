33f862f7ee3d421224da0bea156f4d455c5a5d3e
ragan2 <ragan1@student.gsu.edu>
2017-11-18 23:34:04 -0500

    Update forgotPassword.php

--- forgotPassword.php
+++ forgotPassword.php
@@ -1,28 +1,60 @@
 <?php
-    $active = "Contact";
-    include '_includes/header.php';
+        include '_includes/header.php';
 ?>
 
-<title>
-</title>
-<body>
+<?php
+if(isset($_POST['submit'])){
+    $to = $_POST['email']; // this is the user's Email address
+    $subject = "Reset Password";
+    $message = " A request has been made to reset your password. Please follow the link to reset: http://sonyfortune.com/resetPassword.php";
+    $headers = "From:" . $to;
+    
+    mail($to,$subject,$message,$headers);
+        echo "Mail Sent. Thank you";
+    
+    }
+?>
 
-<br>
-<br>
-<br>
-<div class="container" align="center">
-    <form class="form-inline">
-      <div class="form-group">
-        <label class="sr-only">Email</label>
-        <p class="form-control-static">email@example.com</p>
+
+<!DOCTYPE HTML>
+<head>
+<title>Forgot Password</title>
+</head>
+  <body>
+    <style>
+      .form-area
+      {
+      	padding: auto 40px 60px;
+      	margin: 0px 75px 60px;
+      	}
+        .center {
+        position: absolute;
+        left: 0;
+        right: 0;
+        margin: auto;
+      }
+    </style>
+  
+
+  <div class="container">
+    <br>
+    <br>
+    <h2 style="text-align: center">Forgot your Password? No problem!</h2>
+    <div class="container">
+      <div class="col-md-5 center">
+          <div class="form-area" >
+              <form role="form" action="" method="post">
+              <br style="clear:both">
+                          <h4 style="margin-bottom: 25px; text-align: center;">Enter the email address associated with your DGES.me account</h4>
+          				<div class="form-group">
+      						<input type="text" class="form-control" name="email" placeholder="Email" required>
+      					</div>
+      				  
+                       <input type="submit" name="submit" value="Submit">
+              </form>
+          </div>
+        </div>
       </div>
-    <div class="form-group mx-sm-3">
-      <label for="inputPassword2" class="sr-only">Enter e-mail</label>
-      <input type="password" class="form-control" id="inputPassword2" placeholder="Password">
     </div>
-    <button type="submit" class="btn btn-primary">Reset Password</button>
-    </form>
-  </div>
-</body>
-
+  </body>
 </html>
