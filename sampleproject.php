a18d95e8e8edc2ca25f16c62f2f990ef1e858308
Anthony Leon <knfiore@gmail.com>
2017-11-24 00:21:04 -0500

    sample project

---
+++ sampleproject.php
@@ -0,0 +1,522 @@
+<?php
+    include '_includes/header.php';
+
+	session_start();
+    ?>
+
+
+
+  <!DOCTYPE html>
+<html>
+<head><title>Current Courses</title>
+
+<style>
+
+body {
+
+      background-image:url("https://image.ibb.co/fXc86R/blackboard12.jpg");
+
+  }
+
+.centered {
+  position: fixed;
+  top: 40%;
+  left: 50%;
+  font-size:43px;
+  color: red;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+}
+
+.stroke-3 {
+  stroke-linecap: round;
+}
+
+.button {
+    background-color: #72a347;
+    border: none;
+    color: white;
+    padding: 10px; /*this is how you change size of the circle */
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 24px;
+    margin: 4px 2px;
+    cursor: pointer;
+    {border-radius: 50%;}
+}
+
+
+.M1 {
+
+  position: absolute;
+  top: 44%;
+  left: 20%;
+
+}
+
+.line1 {
+    /*border-left: 6px solid green;*/
+    border-left: 1px dashed #72a347;
+    height: 100px;
+    position: absolute;
+    top: 30%;
+    left: 22.5%;
+
+
+}
+
+
+
+.M2 {
+
+  position: absolute;
+  top: 44%;
+  left: 30%;
+
+}
+
+
+
+.line2 {
+    /*border-left: 6px solid green;*/
+    border-left: 1px dashed #72a347;
+    height: 60px;
+    position: absolute;
+    top: 53%;
+    left: 32.5%;
+
+
+}
+
+
+
+
+.M3 {
+
+  position: absolute;
+  top: 44%;
+  left: 40%;
+
+}
+
+
+
+
+.line3 {
+    /*border-left: 6px solid green;*/
+    border-left: 1px dashed #72a347;
+    height: 100px;
+    position: absolute;
+    top: 30%;
+    left: 42.5%;
+
+
+}
+
+
+.M4 {
+
+  position: absolute;
+  top: 44%;
+  left: 50%;
+
+}
+
+
+
+
+.line4 {
+    /*border-left: 6px solid green;*/
+    border-left: 1px dashed #72a347;
+    height: 60px;
+    position: absolute;
+    top: 53%;
+    left: 52.5%;
+
+
+}
+
+.M5 {
+
+  position: absolute;
+  top: 44%;
+  left: 60%;
+
+}
+
+
+
+.line5 {
+    /*border-left: 6px solid green;*/
+    border-left: 1px dashed #72a347;
+    height: 60px;
+    position: absolute;
+    top: 34%;
+    left: 62.5%;
+
+
+}
+
+.M6 {
+
+  position: absolute;
+  top: 44%;
+  left: 70%;
+
+}
+
+
+
+
+
+.line6 {
+    /*border-left: 6px solid green;*/
+    border-left: 1px dashed #72a347;
+    height: 60px;
+    position: absolute;
+    top: 53%;
+    left: 72.5%;
+
+
+}
+
+
+
+
+.M7 {
+
+  position: absolute;
+  top: 44%;
+  left: 80%;
+
+}
+
+
+
+.line7 {
+    /*border-left: 6px solid green;*/
+    border-left: 1px dashed #72a347;
+    height: 107px;
+    position: absolute;
+    top: 26%;
+    left: 82.5%;
+
+
+}
+
+
+
+
+
+.milestone {
+
+  color: white;
+  font-size: 15px;
+  font-family: sans-serif;
+
+
+}
+
+
+div.mt1 {
+
+  position: absolute;
+  top: 16%;
+  left: 15%;
+  max-width: 150px;
+
+}
+
+div.mt2 {
+
+  position: absolute;
+  top: 65%;
+  left: 25%;
+  max-width: 150px;
+
+}
+
+div.mt3 {
+
+  position: absolute;
+  top: 16%;
+  left: 35%;
+  max-width: 150px;
+
+}
+
+div.mt4 {
+
+  position: absolute;
+  top: 65%;
+  left: 45%;
+  max-width: 150px;
+
+}
+
+div.mt5 {
+
+  position: absolute;
+  top: 16%;
+  left: 55%;
+  max-width: 150px;
+
+}
+
+div.mt6 {
+
+  position: absolute;
+  top: 65%;
+  left: 65%;
+  max-width: 150px;
+
+}
+
+div.mt7 {
+
+  position: absolute;
+  top: 16%;
+  left: 75%;
+  max-width: 150px;
+
+}
+
+
+
+
+
+
+
+ .button5 {border-radius: 50%;}
+
+</style>
+</head>
+<body>
+
+    <h2 class = "centered">______________________________________________</h2>
+
+  <button class="button button5 M1" data-toggle="modal" data-target=".bd-m1-modal-lg">M1</button>
+
+  <div class="modal fade bd-m1-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+
+    <div class="modal-dialog modal-lg">
+
+      <div class="modal-content">
+      Milestone 1:
+Make predictions about ideas for transforming education in Georgia
+​
+Milestone 1 Tasks and Activities:
+Innovation in Education
+What changes are the schools around the world implementing to enhance students experience in the classroom or improve their access to educational content?
+Content:
+Five top takeaways for fostering innovation in education
+Skills for a changing world
+Search the web (e.g., google) for more content
+Think about the new ideas your teachers have introduced in class lately. How do you think teachers or administration decide what changes students might like?
+Content:
+21st Century Classroom​
+5 Big Ways Education will change by 2020
+Complete a short quiz on market research
+  </div>
+
+    </div>
+
+      </div>
+
+  <button class="button button5 M2" data-toggle="modal" data-target=".bd-m2-modal-lg">M2</button>
+
+  <div class="modal fade bd-m2-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+
+    <div class="modal-dialog modal-lg">
+
+      <div class="modal-content">
+
+        Milestone 2:
+Conduct survey across various audiences to inform your predictions
+​
+Milestone 2 Tasks and Activities:
+Challenges in today's education system: Conduct a survey to identify challenges in today's education system. If you could have power to change any one thing, what would that be?
+Define who's the right audience for the survey (e.g., teachers, parents, administration, all of the above)
+Think about how will the input from each of the groups influence your analysis
+​Develop initial list of need to know questions
+In your survey, include questions around your ideas and test the feedback from the audience
+Conduct survey, collect and analyze responses
+​Content:
+Surveys & Samples
+Survey Research Methods
+Designing & Conducting surveys
+Examples of initial need to know
+Bias in survey sampling
+Are students receiving education that they desire? Is there content that students have interest for, but it's not supplied by teachers / schools? Conduct a survey among school students, in various age groups, to understand frustrations that they face and what would they like to change
+Use the steps outlined in the previous task to conduct survey (define your audience, draft questions, conduct survey, collect & analyze results)
+Compare your surveys and think about how the responses from teachers and administration are different from the responses from students. Do teachers see the same problems / gaps as students? Do students accept the gaps, identified by teachers and administration as true gaps?
+Summarize your analysis in a short write-up (use PowerPoint or Word)​​​​
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <button class="button button5 M3" data-toggle="modal" data-target=".bd-m3-modal-lg">M3</button>
+
+  <div class="modal fade bd-m3-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+
+    <div class="modal-dialog modal-lg">
+
+      <div class="modal-content">
+
+        Milestone 3:
+Update your original prediction with insights collected from surveys
+​
+Milestone 3 Tasks and Activities:
+Once you've developed set of survey questions, test it on small subset of your audience
+Use the feedback to refine your survey
+Once you're comfortable with the survey, conduct the survey on wider audience
+Determine what tool do you want to use for your survey (pen/paper, email, online)
+Collect and analyze results
+Content:​
+Data Analysis in Excel
+Data Analysis in R
+Better Customer Insight​
+How to Generate Quick Insights to Improve or Test Product Concepts
+Connecting the dots
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <button class="button button5 M4" data-toggle="modal" data-target=".bd-m4-modal-lg">M4</button>
+
+  <div class="modal fade bd-m4-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+
+    <div class="modal-dialog modal-lg">
+
+      <div class="modal-content">
+
+        Milestone 4:
+Analyze target market size and demographics
+​
+Milestone 4 Tasks and Activities:
+Identify what is the appropriate age group for the company to pilot the program. Think about the following questions: ​
+Should it be younger or older kids? why? ​
+What percent of all students attend each grade? Is it even distribution?
+Should it be in private or public schools? why?
+Should the company target low income or wealthy families?
+How would you define low income families? wealthy families?
+Should the company target high performers or the students that are struggling with grades? why?
+Once you've identified the target market, calculate:
+How many schools, teachers and students fall under your defined group (use the attached spreadsheet for your analysis)
+What's their distribution by region
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <button class="button button5 M5" data-toggle="modal" data-target=".bd-m5-modal-lg">M5</button>
+
+  <div class="modal fade bd-m5-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+
+    <div class="modal-dialog modal-lg">
+
+      <div class="modal-content">
+
+        Milestone 5:
+Draft marketing and communications plan to socialize the new program with the target audience
+​
+Milestone 5 Tasks and Activities:
+TBD
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <button class="button button5 M6" data-toggle="modal" data-target=".bd-m6-modal-lg">M6</button>
+
+  <div class="modal fade bd-m6-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+
+    <div class="modal-dialog modal-lg">
+
+      <div class="modal-content">
+
+        Milestone 6:
+Estimate the cost of your proposed marketing / outreach program
+​
+Milestone 6 Tasks and Activities:
+TBD
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <button class="button button5 M7" data-toggle="modal" data-target=".bd-m7-modal-lg">M7</button>
+
+  <div class="modal fade bd-m7-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+
+    <div class="modal-dialog modal-lg">
+
+      <div class="modal-content">
+
+        Milestone 7:
+Develop final recommendation for the non-profit
+​
+Milestone 7 Tasks and Activities:
+TBD
+
+<br>
+<br>
+
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/4L-BPQVZYXM" frameborder="0" gesture="media" allowfullscreen></iframe>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+
+
+<div class="milestone mt1"><b>Milestone 1:</b>
+Make predictions about ideas for transforming education in Georgia</div>
+
+<div class="milestone mt2"><b>Milestone 2:</b>
+Conduct survey across various audiences to inform your predictions</div>
+
+<div class="milestone mt3"><b>Milestone 3:</b>
+Update your original prediction with insights collected from surveys</div>
+
+<div class="milestone mt4"><b>Milestone 4:</b>
+Analyze target market size and demographics</div>
+
+<div class="milestone mt5"><b>Milestone 5:</b>
+Draft marketing and communications plan to socialize the new program with the target audience</div>
+
+<div class="milestone mt6"><b>Milestone 6:</b>
+Estimate the cost of your proposed marketing / outreach program</div>
+
+<div class="milestone mt7"><b>Milestone 7:</b>
+Develop final recommendation for the non-profit</div>
+
+
+<div class="line1"></div>
+<div class="line2"></div>
+<div class="line3"></div>
+<div class="line4"></div>
+<div class="line5"></div>
+<div class="line6"></div>
+<div class="line7"></div>
+
+
+  </body>
+  </html>
